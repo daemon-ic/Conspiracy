4859f44f92e8e3168fd012f3113617d70d277294
unknown <asewram.business@gmail.com>
2020-12-06 18:32:42 -0500

    logo

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -4,8 +4,15 @@ import React from "react";
 import HomeIcon from "@material-ui/icons/Home";
 import MailIcon from "@material-ui/icons/Mail";
 import PersonIcon from "@material-ui/icons/Person";
+import logo from "../images/raven-logo.png";
 
 export const SidebarData = [
+  {
+    title: "",
+    icon: <img className="logo" src={logo} alt="logo" />,
+    link: "/",
+  },
+
   {
     title: "Home",
     icon: <HomeIcon />,
